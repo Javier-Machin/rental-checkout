c4d11b2f16da5a5a03e0e1be7ee0b6e47b9abe87
Javier Machin <javier.machin@mimecorp.com>
2020-09-07 15:45:01 +0100

    initial commit with babel and webpack config

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value,
+      };
+    });
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <form>
+        <input type="text" value={value} onChange={this.handleChange} />
+      </form>
+    );
+  }
+}
+
+export default App;
