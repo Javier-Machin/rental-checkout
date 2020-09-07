94d48496511c7690291f467382ea2f7c77a110e8
Javier Machin <javier.machin@mimecorp.com>
2020-09-07 16:52:05 +0100

    feat: add test coverage and test-watch

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   moduleDirectories: ['node_modules', __dirname],
   testEnvironment: 'jest-environment-jsdom-sixteen',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/src/serviceWorker.js',
+    '!<rootDir>/src/components/mockHelpers/index.js',
+    '!<rootDir>/src/tests/helpers/index.js',
+    '!<rootDir>/src/index.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 50,
+      lines: 60,
+      statements: 60,
+    },
+  },
+  coverageReporters: ['text', 'html'],
 };
